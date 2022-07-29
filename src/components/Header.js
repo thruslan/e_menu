4047349ba1eabf89eb3e0dd7cf2e4b91282ebf72
thruslan <k4512084@gmail.com>
2022-07-29 17:09:05 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="header__line">
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
